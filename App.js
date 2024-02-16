ed34c1b5a12b8ebea2073659093885587a378088
Chelsea Hannah <80699111+cann9676@users.noreply.github.com>
2024-02-15 21:47:47 -0500

    Test

--- App.js
+++ App.js
@@ -1,10 +1,17 @@
+import React from 'react';
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, useColorScheme } from 'react-native';
 
 export default function App() {
+const colorScheme = useColorScheme();
+
+const themeTextStyle = colorScheme === 'light' ? styles.lightThemeText : styles.darkThemeText;
+const themeContainerStyle = 
+colorScheme === 'light' ? styles.lightContainer : styles.darkContainer;
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+    <View style={[styles.container, themeContainerStyle]}>
+      <Text style={[styles.text, themeTextStyle]}>Color Scheme: {colorScheme}</Text>
       <StatusBar style="auto" />
     </View>
   );
@@ -13,8 +20,19 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  lightContainer: {
+    backgroundColor: '#d0d0c0',
+  },
+  darkContainer: {
+    backgroundColor: '#242c40'
+  },
+  lightThemeText: {
+    color: '#242c40'
+  },
+  darkThemeText: {
+    color: '#d0d0c0'
   },
 });
